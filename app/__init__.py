50a220326528a8b9174d9cc9b2d5a752dafd05d4
Sabesh Bharathi <sabesh5601@gmail.com>
2020-06-07 19:11:21 +0530

    Added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_cors import CORS
 
 
 app = Flask(__name__) #Initialize app as a Flash class for the module 'app' (dir)
+CORS(app)
 app.config.from_object(Config)
 #Now the folder (module) 'app' is registered as the variable 'app'
 db = SQLAlchemy(app) #DB class
